a3ac1680a8f8353e4dec23d417e7e08f53556feb
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-02 16:43:32 +0000

    Tags pnf_gallery 0.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pnf_gallery/tags/pnf_gallery@66552 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ 0.0.0/ext_localconf.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_pnfgallery_pi1.php', '_pi1', 'list_type', 0);
+
+include_once(t3lib_extMgm::extPath($_EXTKEY) . 'Classes/user_pnfgalleryOnCurrentPage.php'); // Conditions for JS and CSS including
+
+tx_pnfgallery_sources_manager::subscribe('tx_pnfgallery_dam');
+tx_pnfgallery_themes_manager::subscribe('tx_pnfgallery_simplegallery');
+tx_pnfgallery_themes_manager::subscribe('tx_pnfgallery_widget');
+
+?>
