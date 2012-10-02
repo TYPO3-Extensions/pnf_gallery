568eadeba92e9c123d56f3ded869dddb76469992
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-02 16:44:42 +0000

    Tags pnf_gallery_share 0.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pnf_gallery/tags/pnf_gallery_share@66553 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ 0.0.0/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "pnf_gallery_share".
+#
+# Auto generated 23-08-2012 15:46
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Partage d\'une galerie',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Plan Net',
+	'author_email' => 'technique@in-cite.net',
+	'shy' => '',
+	'dependencies' => 'pnf_gallery,sexybookmarks',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'pnf_gallery' => '',
+			'sexybookmarks' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"06c6";s:12:"ext_icon.gif";s:4:"1bdc";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"d98f";s:20:"doc/wizard_form.html";s:4:"ab38";}',
+);
+
+?>
