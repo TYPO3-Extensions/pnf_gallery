c52c4f404b26bdf6df731a9482f528b395fd37f5
emilieprudhomme <emilieprudhomme@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-15 14:50:57 +0000

    Add new extension: - pnf_gallery_damcat: add DAM category utilisation to source DAM (flexform configuration, new markers and filters) - pnf_gallery_dam_order: add possibility to place important images to first in gallery (use integer field to DAM records)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pnf_gallery/trunk/pnf_gallery_damcat@72976 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pnf_gallery_damcat".
+ *
+ * Auto generated 12-03-2013 12:28
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Filtre sur les catégories du DAM pour la galerie',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Plan.Net France',
+	'author_email' => 'typo3@plan-net.fr',
+	'shy' => '',
+	'dependencies' => 'pnf_gallery,dam_catedit',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'pnf_gallery' => '',
+			'dam_catedit' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"76c0";s:12:"ext_icon.gif";s:4:"1bdc";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"76d1";s:20:"doc/wizard_form.html";s:4:"028d";}',
+);
+
+?>
